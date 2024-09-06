408ee6ca33101e1e039112f1d085dd5d0ebe2cba
Logan Girouard <115243538+lgirouard@users.noreply.github.com>
2024-09-06 17:03:58 -0300

    Update Repo and Service to use ApplianceEntity

--- src/main/java/com/appliances/repositories/ApplianceRepo.java
+++ src/main/java/com/appliances/repositories/ApplianceRepo.java
@@ -1,7 +1,10 @@
 package com.appliances.repositories;
 
 import com.appliances.entities.ApplianceEntity;
+import com.appliances.entities.IAppliance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ApplianceRepo extends JpaRepository<ApplianceEntity, Long> {
 }
--- src/main/java/com/appliances/services/ApplianceService.java
+++ src/main/java/com/appliances/services/ApplianceService.java
@@ -1,8 +1,37 @@
 package com.appliances.services;
 
+import com.appliances.entities.ApplianceEntity;
+import com.appliances.entities.IAppliance;
+import com.appliances.repositories.ApplianceRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ApplianceService {
 
+    @Autowired
+    private ApplianceRepo applianceRepo;
+
+    public String turnPowerOn(Long id){
+        Optional<ApplianceEntity> appliance = applianceRepo.findById(id);
+
+        if(appliance.isPresent()){
+            appliance.get().powerOn();
+            applianceRepo.save(appliance.get());
+            return appliance.get().getStatus();
+        }
+        return "Appliance not found!";
+    }
+    public String turnPowerOff(Long id){
+        Optional<ApplianceEntity> appliance = applianceRepo.findById(id);
+
+        if(appliance.isPresent()){
+            appliance.get().powerOff();
+            applianceRepo.save(appliance.get());
+            return appliance.get().getStatus();
+        }
+        return "Appliance not found!";
+    }
 }
