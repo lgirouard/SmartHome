8434babaa7fe519b78ed5b7b731d4b8ac7cb894c
Logan Girouard <115243538+lgirouard@users.noreply.github.com>
2024-09-06 21:26:05 -0300

    Add Icons for appliances

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,6 +13,22 @@ function setFanSpeed(speed) {
         .catch(error => console.error('Error:', error));
 }
 
+function updateFanSpeed() {
+    const speed = parseInt(document.getElementById("fan-speed-slider").value);
+    const fanIcon = document.getElementById("fan-icon");
+    if (speed === 0) {
+        fanIcon.src = "/img/fan.svg";
+        document.getElementById("fan-status").innerText = "Fan Status: Off";
+    } else if (speed === 1) {
+        fanIcon.src = "/img/fan-speed-1.svg";
+        document.getElementById("fan-status").innerText = "Fan Status: Low Speed";
+    } else if (speed === 2) {
+        fanIcon.src = "/img/fan-speed-2.svg";
+        document.getElementById("fan-status").innerText = "Fan Status: High Speed";
+    }
+    setFanSpeed(speed);
+}
+
 // Generic function to turn off any appliance by ID
 function turnOffAppliance(id) {
     fetch(`${API_URL}/${id}/off`, { method: 'POST' })
@@ -20,9 +36,13 @@ function turnOffAppliance(id) {
         .then(data => {
             if (id === lightId) {
                 document.getElementById("light-status").innerText = `Light Status: ${data}`;
+                document.getElementById("light-icon").src = "/img/light-switch-off.svg";
+
             } else if (id === fanId) {
                 document.getElementById("fan-status").innerText = `Fan Status: ${data}`;
             } else if (id === acId) {
+
+                document.getElementById("ac-status").classList.add("black");
                 document.getElementById("ac-status").innerText = `AC Status: ${data}`;
             }
         })
@@ -36,9 +56,11 @@ function turnOnAppliance(id) {
         .then(data => {
             if (id === lightId) {
                 document.getElementById("light-status").innerText = `Light Status: ${data}`;
+                document.getElementById("light-icon").src = "/img/light-switch-on.svg";
             } else if (id === fanId) {
                 document.getElementById("fan-status").innerText = `Fan Status: ${data}`;
             } else if (id === acId) {
+                document.getElementById("ac-status").classList.remove("black");
                 document.getElementById("ac-status").innerText = `AC Status: ${data}`;
             }
         })
@@ -47,9 +69,27 @@ function turnOnAppliance(id) {
 // Air Conditioner Control
 function setACTemperature() {
     const temperature = document.getElementById("temperature-input").value;
+
+    const acStatus = document.getElementById("ac-status");
+    acStatus.classList.remove("black");
+    if (temperature === "") {
+        acStatus.innerText = "Please set a valid temperature.";
+        return;
+    }
+
     fetch(`${API_URL}/airconditioner/${acId}/temperature?temperature=${temperature}`, { method: 'POST' })
         .then(response => response.text())
         .then(data => {
+
+            if (temperature < 20) {
+                acStatus.innerText = `AC Status: Cold (${temperature}°C)`;
+                acStatus.classList.remove("warm");
+                acStatus.classList.add("cold");
+            } else {
+                acStatus.innerText = `AC Status: Warm (${temperature}°C)`;
+                acStatus.classList.remove("cold");
+                acStatus.classList.add("warm");
+            }
             document.getElementById("ac-status").innerText = `AC Status: ${data}`;
         })
         .catch(error => console.error('Error:', error));
